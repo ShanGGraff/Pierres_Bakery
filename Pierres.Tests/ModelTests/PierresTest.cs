59989f644d62ab706c83e99180e60de2c7066145
Shane Graff <copellius@gmail.com>
2021-09-14 11:56:54 -0700

    Confirm testing is set up correctly. No real tests yet

---
+++ Pierres.Tests/ModelTests/PierresTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pierres;
+
+namespace Pierres.Tests
+{
+  [TestClass]
+  public class PierresTests
+  {
+    [TestMethod]
+    public void DoesBreadNumber_ConatainAnInt_True()
+    {
+      //testing code will go here
+    }
+  }
+}
