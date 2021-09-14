b2670413da101352023570338cf4c24c2e78c37a
Shane Graff <copellius@gmail.com>
2021-09-14 12:17:47 -0700

    Add first failing test for Bread object

--- Pierres.Tests/ModelTests/PierresTest.cs
+++ Pierres.Tests/ModelTests/PierresTest.cs
@@ -9,6 +9,8 @@ namespace Pierres.Tests
     [TestMethod]
     public void DoesBreadNumber_ConatainAnInt_True()
     {
+      Bread testBread = new Bread();
+      Assert.AreEqual(true, testBread.NumberOfBreads(2));
       //testing code will go here
     }
   }
