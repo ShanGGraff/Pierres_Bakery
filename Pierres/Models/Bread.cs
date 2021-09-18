5f7464bed552c7a199b6f0574cf7712f9b74504a
Shane Graff <copellius@gmail.com>
2021-09-18 14:33:17 -0700

    Add BreadPastryCost class

--- Pierres/Models/Bread.cs
+++ Pierres/Models/Bread.cs
@@ -2,17 +2,15 @@ namespace Pierres
 {
   public class Bread
   {
+
     public int NumberOrdered { get; }
 
       public Bread(int numberOrdered)
       {
         NumberOrdered = numberOrdered;
       }
-    // public int NumberOfBreads(int number_ordered)
-    // {
-      
-    //   return number_ordered;
-    // }
     // Bread: Buy 2, get 1 free (every 3rd loaf of bread is free.). A single loaf costs $5, two loaves costs $10, and three loaves cost $10.
+    
+
   }
 }
