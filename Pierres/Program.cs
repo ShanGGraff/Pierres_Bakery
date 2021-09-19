ad8d1d1d60c486a9c31e9d3ef9c2fac97114d320
Shane Graff <copellius@gmail.com>
2021-09-19 07:52:27 -0700

    Add Bread and Pastry objects to main

--- Pierres/Program.cs
+++ Pierres/Program.cs
@@ -7,9 +7,27 @@ namespace Pierres_Bakery
   {
     public static void Main()
     {
-      Console.WriteLine("Hello, Welcome to Pierre's Bakery");
-      Console.WriteLine("Here are our bread prices: A single loaf costs $5, two loaves costs $10, and three loaves cost $10.");
-      Console.WriteLine("Here are our pastry prices: Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10");
+      Console.WriteLine("      Hello, Welcome to Pierre's Bakery!");
+      Console.WriteLine("");
+      Console.WriteLine(@"      Here are our bread prices: 
+      A single loaf costs $5, two loaves cost $10, and three 
+      loaves cost $10.");
+      Console.WriteLine("");
+      Console.WriteLine(@"      Here are our pastry prices: 
+      Buy 1 for $2 or 3 for $5. Four pastries cost $7, 
+      five pastries cost $9, and six pastries cost $10");
+      Console.WriteLine("");
+      Console.WriteLine("      How many loaves of bread would you like? Please choose 0-3.");
+      int numberOfBread = Int16.Parse(Console.ReadLine());
+      Bread newBread = new Bread(numberOfBread);
+      Console.WriteLine(newBread.totalCostOfBreads());
+      Console.WriteLine("");
+      Console.WriteLine("      How many pastries would you like? Please choose 0, 1, 3, 4, 5 or 6.");
+      int numberOfPastries = Int16.Parse(Console.ReadLine());
+      Pastry newPastry = new Pastry(numberOfPastries);
+      Console.WriteLine(newPastry.totalCostOfPastries());
+      
+
     }
   }
 }
