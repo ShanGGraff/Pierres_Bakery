d6cbaa6beb65c43da84fe61810451fe4fc752f38
Shane Graff <copellius@gmail.com>
2021-09-18 21:20:02 -0700

    Add conditional for totalcost for 1 pastry ordered. Test passing

--- Pierres/Models/Pastry.cs
+++ Pierres/Models/Pastry.cs
@@ -4,16 +4,18 @@ namespace Pierres.Models
   {
     public int NumberOrdered { get; }
 
-      public Pastry(int numberOrdered)
-      {
-        NumberOrdered = numberOrdered;
-      }
+    public Pastry(int numberOrdered)
+    {
+      NumberOrdered = numberOrdered;
+    }
       // Pastry: Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.
-      //  public int totalCostOfPastries()
-      // {
-      //   int totalCost = 0;
-      //   if (numberedOrdered == 1)
-      //     return (2);
-      // }
+    public int totalCostOfPastries()
+    {
+      if (NumberOrdered == 1)
+      {
+        return 2;
+      }
+      return 0;  
+    }
   }
 }
